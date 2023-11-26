c5a0e960c1ca1e9144337e2ab0b8b158f4593590
GiacWebDev <giacobechini@gmail.com>
2023-11-26 15:18:21 +0100

    [edit] info in show

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -1,5 +1,17 @@
 @extends('layouts.main')
 
 @section('content')
-    <h1>{{ $comic->title }}</h1>
+    <div class="row">
+        <div class="card text-center">
+            <div class="card-header">
+                <h3>Comic</h3>
+                <img src="{{ $comic->thumb }}" class="card-img-top" alt="{{ $comic->title }}">
+            </div>
+            <div class="card-body">
+                <h1>{{ $comic->title }}</h1>
+                <p class="card-text">{{ $comic->description }}</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+    </div>
 @endsection
